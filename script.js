5b84750e160994a6cfd1a3e301540eaaed6084dd
Asia <misakamikotocsr@gmail.com>
2017-03-23 16:29:08 -0500

    more js stuff

--- script.js
+++ script.js
@@ -1,7 +1,28 @@
-var firstName = prompt("What is your first name?");
-var lastName = prompt("What is your last name?");
-var userAge = prompt("What is your age?");
-var fullName = firstName + " " + lastName;
+// var firstName = prompt("What is your first name?");
+// var lastName = prompt("What is your last name?");
+// var userAge = prompt("What is your age?");
+// var fullName = firstName + " " + lastName;
+//
+// alert("Hello there, " + fullName);
+// alert("You are " + userAge + " years old");
 
-alert("Hello there, " + fullName);
-alert("You are " + userAge + " years old");
+// If age is negative
+if(age < 0) {
+  console.log("Come back when you're out of the womb");
+}
+
+// If age is 21
+if(age === 21) {
+  console.log("Happy 21st Birthday!");
+}
+
+// If age is odd
+// (not evenly divisible by two)
+if(age % 2 !== 0) {
+  console.log("Your age is odd!");
+}
+
+// If age is a perfect square
+if(age % Math.sqrt(age) === 0) {
+  console.log("Your age is a perfect square!");
+}
